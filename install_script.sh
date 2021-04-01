a9fd0deea1ccad68f34f30bb60532c8db8beb2f0
Manish Reddy <mani-reddy@outlook.com>
2021-04-01 12:57:00 +0100

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -4,9 +4,13 @@
 sudo apt install awscli
 
 #install kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-chmod +x kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+# curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+# chmod +x kubectl
+# sudo mv ./kubectl /usr/local/bin/kubectl
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
 
 #install eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz"| tar xz -C /tmp
